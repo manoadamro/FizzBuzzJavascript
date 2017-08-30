c2cc627fe55093eadd53fca8fa4492ca31c4a7df
Adam Romano <manoadamro.imp@gmail.com>
2017-08-30 10:21:33 +0100

    tests written

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,29 @@
+
+
+describe ('FizzBuzz', function () {
+
+  describe ('Range', function () {
+
+    it ('Prints all numbers from 1 to 100', function () {
+      
+    });
+
+  });
+
+  describe ('Return Value', function () {
+
+    it ('Returns Fizz when divisible by 3 and not 5', function () {
+
+    });
+
+    it ('Returns Buzz when divisible by 5 and not 3', function () {
+
+    });
+
+    it ('Returns FizzBuzz when divisible by 3 and 5', function () {
+
+    });
+
+  });
+
+});
